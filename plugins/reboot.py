d7546ee2960847d26113e82c91c21966eb8fb018
cod lucas <100681227+COD-LUCAS@users.noreply.github.com>
2025-11-21 10:18:50 +0530

    Update reboot.py

--- plugins/reboot.py
+++ plugins/reboot.py
@@ -4,7 +4,12 @@ import sys
 
 def register(bot):
 
-    @bot.on(events.NewMessage(pattern=r"\/reboot"))
+    @bot.on(events.NewMessage(pattern=r"^/reboot$"))
     async def reboot(event):
+
+        # OWNER CHECK
+        if event.sender_id != bot.owner_id:
+            return await event.reply("❌ Only owner can reboot the bot.")
+
         await event.reply("🔄 Rebooting…")
         os.execv(sys.executable, [sys.executable] + sys.argv)
