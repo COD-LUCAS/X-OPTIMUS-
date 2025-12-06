81c3003badb2554dc875fd756f8d1fd8534e85c7
cod lucas <100681227+COD-LUCAS@users.noreply.github.com>
2025-12-06 20:34:31 +0530

    Update ping.py

--- plugins/ping.py
+++ plugins/ping.py
@@ -2,13 +2,21 @@ from telethon import events
 import time
 
 def register(bot):
-    
+
     @bot.on(events.NewMessage(pattern=r"^/ping$"))
     async def ping(event):
+
+        uid = event.sender_id
+        mode = bot.mode.lower()
+
+        if mode == "private":
+            if uid != bot.owner_id and uid not in bot.sudo_users:
+                return
+
         start = time.time()
         msg = await event.reply("Pinging...")
         end = time.time()
-        
+
         ping_time = round((end - start) * 1000, 2)
-        
+
         await msg.edit(f"Response time: {ping_time} ms")
