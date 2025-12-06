78aa061538802d9ae420d111f55ddaa0163d1d51
cod lucas <100681227+COD-LUCAS@users.noreply.github.com>
2025-12-06 14:02:16 +0530

    Update startup.py

--- plugins/startup.py
+++ plugins/startup.py
@@ -1,24 +1,33 @@
+import os
 from telethon import events
-from datetime import datetime
 
-def build_ui():
+def get_mode():
+    # AUTO-DETECT MODE
+    if os.getenv("PUBLIC_MODE") in ["False", "0", "false"]:
+        return "Private"
+    if os.getenv("OWNER_ONLY") in ["True", "1", "true"]:
+        return "Private"
+    return "Public"
+
+def startup_text():
+    mode = get_mode()
+
     return (
-        "*_X-OPTIMUS STARTED!_* \n\n"
-        "_Mode         :_ *Public*\n"
-        "_Language     :_ *English*\n"
-        "_Handlers     :_ *.,*\n"
-        "_Status       :_ *Online*\n"
-        "_Engine       :_ *Active*"
+        "🔥 𝗫-𝗢𝗣𝗧𝗜𝗠𝗨𝗦 𝗢𝗡𝗟𝗜𝗡𝗘\n\n"
+        f"▪ 𝗠𝗼𝗱𝗲       : {mode}\n"
+        "▪ 𝗟𝗮𝗻𝗴𝘂𝗮𝗴𝗲   : 𝗘𝗻𝗴𝗹𝗶𝘀𝗵\n"
+        "▪ 𝗦𝘁𝗮𝘁𝘂𝘀     : 𝗢𝗻𝗹𝗶𝗻𝗲\n"
+        "▪ 𝗛𝗮𝗻𝗱𝗹𝗲𝗿𝘀   : 𝗟𝗼𝗮𝗱𝗲𝗱"
     )
 
 async def on_startup(bot):
     try:
-        await bot.send_message("me", build_ui())
+        await bot.send_message("me", startup_text())
     except:
         pass
 
 def register(bot):
 
     @bot.on(events.NewMessage(pattern=r"^/startup$"))
-    async def manual_start(event):
-        await event.reply(build_ui())
+    async def startup(event):
+        await event.reply(startup_text())
