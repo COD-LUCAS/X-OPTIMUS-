09268550b83d38411938593395bd212c0e4eac95
cod lucas <100681227+COD-LUCAS@users.noreply.github.com>
2025-12-06 20:35:20 +0530

    Update rbg.py

--- plugins/rbg.py
+++ plugins/rbg.py
@@ -3,18 +3,25 @@ import requests
 from telethon import events
 
 CONFIG_KEY = "REMOVE_BG_API_KEY"
-CONFIG_FILE = "container_data/config.env"
 
 def register(bot):
 
     @bot.on(events.NewMessage(pattern=r"^/rbg$"))
     async def rbg(event):
+
+        uid = event.sender_id
+        mode = bot.mode.lower()
+
+        if mode == "private":
+            if uid != bot.owner_id and uid not in bot.sudo_users:
+                return
+
         api = os.getenv(CONFIG_KEY, "")
 
         if not api:
             return await event.reply(
                 "❌ REMOVE_BG_API_KEY is not set.\n\n"
-                "**Set it using:**\n"
+                "Set it using:\n"
                 "`/setvar REMOVE_BG_API_KEY=your_api_key_here`"
             )
 
