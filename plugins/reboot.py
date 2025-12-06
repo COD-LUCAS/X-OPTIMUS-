0e924890289ca1f0b7e889ff2cd15e522cc3a3e9
cod lucas <100681227+COD-LUCAS@users.noreply.github.com>
2025-12-06 19:21:33 +0530

    Update reboot.py

--- plugins/reboot.py
+++ plugins/reboot.py
@@ -7,9 +7,10 @@ def register(bot):
     @bot.on(events.NewMessage(pattern=r"^/reboot$"))
     async def reboot(event):
 
-        # OWNER CHECK
-        if event.sender_id != bot.owner_id:
-            return await event.reply("❌ Only owner can reboot the bot.")
+        uid = event.sender_id
+
+        if uid != bot.owner_id and uid not in bot.sudo_users:
+            return await event.reply("❌ Permission denied.")
 
         await event.reply("🔄 Rebooting…")
         os.execv(sys.executable, [sys.executable] + sys.argv)
